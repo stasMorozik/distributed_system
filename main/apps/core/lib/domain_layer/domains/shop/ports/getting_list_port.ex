7a252af939107fc35663c9ab555191d4edd085a2
stasMorozik <stanislav.morozov1107@gmail.com>
2022-06-21 22:49:06 +0300

    domain shop

--- main/apps/core/lib/domain_layer/domains/shop/ports/getting_list_port.ex
+++ main/apps/core/lib/domain_layer/domains/shop/ports/getting_list_port.ex
@@ -1,3 +1,23 @@
 defmodule Core.DomainLayer.Domains.Shop.Ports.GettingListPort do
+  @moduledoc false
 
+  alias Core.DomainLayer.Domains.Shop.ShopEntity
+
+  alias Core.DomainLayer.Common.ValueObjects.Pagination
+  alias Core.DomainLayer.Common.ValueObjects.Sorting
+  alias Core.DomainLayer.Common.ValueObjects.Filtration
+  alias Core.DomainLayer.Common.Dtos.ImpossibleCallError
+  alias Core.DomainLayer.Common.Dtos.ImpossibleGetError
+
+  @type t :: Module
+
+  @type ok :: {:ok, nonempty_list(ShopEntity.t())}
+
+  @type error :: {
+          :error,
+          ImpossibleCallError.t()
+          | ImpossibleGetError.t()
+        }
+
+  @callback get(Pagination.t(), Sorting.t(), Filtration.t())
 end
